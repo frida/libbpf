a5508305703bb93ee3c1bb109ca3b59e1600e39e
Ole André Vadla Ravnås <oleavr@frida.re>
2026-02-04 15:46:34 +0100

    include: Avoid Android NDK compiler.h conflict

--- include/linux/compiler.h
+++ include/linux/compiler.h
@@ -3,6 +3,10 @@
 #ifndef __LINUX_COMPILER_H
 #define __LINUX_COMPILER_H
 
+#ifdef __ANDROID__
+# include_next <linux/compiler.h>
+#endif
+
 #define likely(x)		__builtin_expect(!!(x), 1)
 #define unlikely(x)		__builtin_expect(!!(x), 0)
 
